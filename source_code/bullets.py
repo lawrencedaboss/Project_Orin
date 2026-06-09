def49d10f24d3d97538a13b54ac2857f550737aa
justindang-ux <justin.dang@misd400.org>
2026-06-08 19:38:38 -0700

    Modify Bullet class for wall collision and speed change
    Updated Bullet class to include wall collision handling and adjusted speed.

--- source_code/bullets.py
+++ source_code/bullets.py
@@ -2,7 +2,7 @@ import pygame
 import math
 
 class Bullet:
-    def __init__(self, x, y, vx, vy, speed=600, radius=4, color=(255, 220, 50), loading_zone_x=0, loading_zone_y=0,collide=True):
+    def __init__(self, x, y, vx, vy, speed=900, radius=4, color=(255, 220, 50), loading_zone_x=0, loading_zone_y=0,collidemonster=True,collidewall=True):
         self.x = x
         self.y = y
         # velocity is normalized direction multiplied by speed
@@ -14,15 +14,22 @@ class Bullet:
         self.radius = radius
         self.color = color
         self.rect = pygame.Rect(int(x - radius), int(y - radius), radius * 2, radius * 2)
-        self.collide=collide
+        self.collidemonster = collidemonster
+        self.collidewall = collidewall  
+        self.walltime=0
     def update(self, dt):
-        self.x += self.vx * dt
-        self.y += self.vy * dt
+        self.x += self.vx * dt*(self.life+1)/3
+        self.y += self.vy * dt*(self.life+1)/3
         self.life -= dt
         self.rect.x = int(self.x - self.radius)
         self.rect.y = int(self.y - self.radius)
         return self.life > 0
-
+    def check_wall_collision(self,dt):
+        if not self.collidewall:
+            self.walltime+=dt
+            if self.walltime>0.1:
+                self.collidewall=True
+                self.walltime=0
 
     def draw(self, screen):
         self.rect=pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
