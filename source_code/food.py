17da40df198fdc1fa561f1478bb66592404bb4e7
justindang-ux <justin.dang@misd400.org>
2026-06-11 16:38:05 -0700

    Add nutrition attribute to Food class

--- source_code/food.py
+++ source_code/food.py
@@ -1,4 +1,6 @@
 import pygame
+
+from config import FOOD_HUNGER_RESTORE
 class Food:
     def __init__(self, x, y, size, loading_zone_x, loading_zone_y):
         self.x = x
@@ -6,6 +8,7 @@ class Food:
         self.size = size
         self.loading_zone_x = loading_zone_x
         self.loading_zone_y = loading_zone_y
+        self.nutrition = FOOD_HUNGER_RESTORE
         self.rect = pygame.Rect(self.x, self.y, self.size, self.size)
     def update(self, dt):
         pass
